955302d0bca9a35684c3462ce1e55ac876970165
syartseva <sofia.yartseva@gmail.com>
2026-07-15 17:16:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'psy-trainer-v1';
+const CACHE = 'psy-trainer-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
